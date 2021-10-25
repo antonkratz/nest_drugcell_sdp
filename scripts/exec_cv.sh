03af1b640b5f099f462d8856e4646c6b4b6aae36
Akshat Singhal <a2singhal@ucsd.edu>
2021-10-25 01:02:36 -0700

    script change dump

--- scripts/exec_cv.sh
+++ scripts/exec_cv.sh
@@ -10,11 +10,11 @@ drugs=`awk 'BEGIN {print $1}' "${homedir}/data/drugname_${dataset}.txt"`
 
 for ont in ctg
 do
-	for drug in Palbociclib
+	for drug in "nutlin-3A" "paclitaxel" 
 	do
 		for ((i=1;i<=folds;i++));
 		do
-			#bash "${homedir}/scripts/create_cv_data.sh" $homedir $dataset $drug $folds $i
+			bash "${homedir}/scripts/create_cv_data.sh" $homedir $dataset $drug $folds $i
 			sbatch -J "NDC_${ont}_${drug}_${i}" -o "${homedir}/logs/out_${ont}_${drug}_${i}.log" ${homedir}/scripts/cv_batch.sh $homedir $ont $dataset $drug ${zscore_method} $i
         done
 	done
